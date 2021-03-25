daafa70d71437ec959a4dc43722763a3044229ca
Jaret Screws <jaretscrews@gmail.com>
2021-03-25 00:26:38 -0400

    testing out the parsing of errors

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@
 
 #include <iostream>
 
-int main(){
+int main(int argc, char **argv) {
     StackOverflowApi x;
     TerminalReader t;
-    auto res = t.excuteCommandAndCaptureOutput("mkdir testBuild && cd testBuild && cmake ../.. && make");
-    std::cout << res << std::endl;
+    std::string cmd;
+    for(int i = 1; i < argc; i++) {
+        cmd.append(argv[i]);
+        if(i != argc - 1)
+            cmd.append(" ");
+    }
+    std::cout << "cmd: " << cmd << std::endl;
+    auto res = t.excuteCommandAndCaptureOutput(cmd);
+    auto errors = t.parseForErrors(res);
+    std::cout << errors << std::endl;
     //x.SearchForTopic("test");
 }
